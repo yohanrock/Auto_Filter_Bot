b97369df544d635421601e4ff35bff94f64d389e
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-08-02 00:45:58 +0530

    pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1615,7 +1615,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     ),
                     parse_mode=enums.ParseMode.HTML,
                     reply_markup=InlineKeyboardMarkup([[
-                        InlineKeyboardButton("🚀 Buy Premium 🚀", callback_data="premium_info")
+                        InlineKeyboardButton("♻️ Buy Premium ♻️", callback_data="premium_info")
                     ]])
                 )
                 await asyncio.sleep(DELETE_TIME)
