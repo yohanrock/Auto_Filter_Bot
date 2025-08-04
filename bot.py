3d57bf9e978f2e051ca7e1095ad1243477f120d7
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-08-04 15:50:36 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,8 @@ from plugins import web_server, check_expired_premium, keep_alive
 from dreamxbotz.Bot import dreamxbotz
 from dreamxbotz.util.keepalive import ping_server
 from dreamxbotz.Bot.clients import initialize_clients
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = 500_000_000
 
 import logging
 import logging.config
