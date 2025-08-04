2340ff6025f247a673fb357e2052660f2a55bcf8
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-08-04 15:40:49 +0530

    channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -476,7 +476,7 @@ def generate_movie_message(movie_doc, base_name):
 
         epi_str = "\n".join(episode_lines)
         if epi_str:
-            epi_block = f"📺 ᴇᴘɪsᴏᴅᴇs : <b>\n{epi_str}</b>"
+            epi_block = f"📺 ᴇᴘɪsᴏᴅᴇs : <b>{epi_str}</b>"
 
     genres = movie_doc.get("genres", "N/A")
     quality_str = ", ".join(sorted(all_qualities)) if all_qualities else "N/A"
