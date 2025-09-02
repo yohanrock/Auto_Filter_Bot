c2697c6111a6e49745b2ffc01c942261bb2db54e
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-09-02 14:55:40 +0530

    commands.py ✅

--- plugins/commands.py
+++ plugins/commands.py
@@ -171,6 +171,9 @@ async def start(client, message):
         if referdb.is_user_in_list(message.from_user.id):
             await message.reply_text("Yᴏᴜ ʜᴀᴠᴇ ʙᴇᴇɴ ᴀʟʀᴇᴀᴅʏ ɪɴᴠɪᴛᴇᴅ ❗")
             return
+        if await db.is_user_exist(message.from_user.id): 
+            await message.reply_text("‼️ Yᴏᴜ Hᴀᴠᴇ Bᴇᴇɴ Aʟʀᴇᴀᴅʏ Iɴᴠɪᴛᴇᴅ ᴏʀ Jᴏɪɴᴇᴅ")
+            return
         try:
             uss = await client.get_users(user_id)
         except Exception:
