056202f2b912d4c016a761ec4452f801e9c1d099
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-09-09 16:46:22 +0530

    Update pmfilter.py ✅

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1461,7 +1461,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         try:
             user_id = query.from_user.id
             username = query.from_user.mention
-            log_msg = await client.send_cached_media(chat_id=LOG_CHANNEL, file_id=file_id,)
+            log_msg = await client.send_cached_media(chat_id=BIN_CHANNEL, file_id=file_id,)
             fileName = {quote_plus(get_name(log_msg))}
             dreamx_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
             dreamx_download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
