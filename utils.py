9ab0429e4801f469f04e218b77cf51a095654ae9
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-09-09 16:57:18 +0530

    utils.py ✅

--- utils.py
+++ utils.py
@@ -615,7 +615,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                 f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
                 f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n\n"
                 f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or '𝐈𝐍𝐅𝐈𝐍𝐈𝐓𝐘 MOVIES'}\n</b>"
             )
             cap += "\n\n🧾 <u>Your Requested Files Are Here</u> 👇\n\n</b>"
             for idx, file in enumerate(files, start=offset):
