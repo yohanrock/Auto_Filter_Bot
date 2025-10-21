630079725f2447722241eff7e767f47a87c18818
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-10-21 10:44:37 +0530

    Update shortener API keys and website URLs

--- info.py
+++ info.py
@@ -106,14 +106,14 @@ TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/shortnerlinksolve/26")   # 
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/shortnerlinksolve/26")   # Third tutorial link for verification
 
 # Verification (Must Fill All Veriables. Else You Got Error
-SHORTENER_API = environ.get("SHORTENER_API", "H4oMG4h8TfXwkOFLeabYSIwzaUL2") # Shortener API key
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "shareus.io") # Shortener website
+SHORTENER_API = environ.get("SHORTENER_API", "03a1f31a88c44180200c606b9903b37404440f16") # Shortener API key
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "gplinks.com") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "H4oMG4h8TfXwkOFLeabYSIwzaUL2")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "shareus.io") # Shortener website for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "03a1f31a88c44180200c606b9903b37404440f16")  # Shortener API key for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "gplinks.com") # Shortener website for second website
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "H4oMG4h8TfXwkOFLeabYSIwzaUL2")  
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "shareus.io") # Shortener website for third website
+SHORTENER_API3 = environ.get("SHORTENER_API3", "03a1f31a88c44180200c606b9903b37404440f16")  
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "gplinks.com") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))    
@@ -169,7 +169,7 @@ AUTH_REQ_CHANNELS = [int(ch) for ch in auth_req_channels.split() if ch and id_pa
 AUTH_CHANNELS = [int(ch) for ch in auth_channels.split() if ch and id_pattern.match(ch)]
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-LANGUAGES = {"ᴍᴀʟᴀʏᴀʟᴀᴍ":"mal","ᴛᴀᴍɪʟ":"tam","ᴇɴɢʟɪsʜ":"eng","ʜɪɴᴅɪ":"hin","ᴛᴇʟᴜɢᴜ":"tel","ᴋᴀɴɴᴀᴅᴀ":"kan","ɢᴜᴊᴀʀᴀᴛɪ":"guj","ᴍᴀʀᴀᴛʜɪ":"mar","ᴘᴜɴᴊᴀʙɪ":"pun"}
+LANGUAGES = {"ᴛᴇʟᴜɢᴜ":"tel","ᴛᴀᴍɪʟ":"tam","ᴇɴɢʟɪsʜ":"eng","ʜɪɴᴅɪ":"hin","ᴍᴀʟᴀʏᴀʟᴀᴍ":"mal","ᴋᴀɴɴᴀᴅᴀ":"kan","ɢᴜᴊᴀʀᴀᴛɪ":"guj","ᴍᴀʀᴀᴛʜɪ":"mar","ᴘᴜɴᴊᴀʙɪ":"pun"}
 QUALITIES = ["360P", "480P", "720P", "1080P", "1440P", "2160P", "4K"]
 
 SEASON_COUNT = 12
