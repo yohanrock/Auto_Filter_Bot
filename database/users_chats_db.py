66393a9139b890c3a54cbda16be7e68928edef2f
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-10-30 14:01:27 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -346,7 +346,7 @@ class Database:
 
     async def give_free_trial(self, user_id):
         user_id = user_id
-        seconds = 5*60         
+        seconds = 2         
         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
         user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
         await self.users.update_one({"id": user_id}, {"$set": user_data}, upsert=True)
